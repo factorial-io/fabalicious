4bd87e754561c8695120289500e8e4a8bf945aab
Stephan Maximilian Huber <hello@stephanmaximilianhuber.com>
2014-10-07 14:48:00 +0200

    silence output, reset after install

--- fabfile.py
+++ fabfile.py
@@ -517,12 +517,14 @@ def install():
         run('chmod u+w '+env.config['siteFolder'])
         run('chmod u+w '+env.config['siteFolder']+'/settings.php')
         run('rm -f '+env.config['siteFolder']+'/settings.php.old')
-        run('mv '+env.config['siteFolder']+'/settings.php '+env.config['siteFolder']+'/settings.php.old')
+        run('mv '+env.config['siteFolder']+'/settings.php '+env.config['siteFolder']+'/settings.php.old 2>/dev/null')
         sites_folder = os.path.basename(env.config['siteFolder'])
         run_drush('site-install minimal  --sites-subdir='+sites_folder+' --site-name="'+settings['name']+'" --account-name=admin --account-pass=admin --db-url=mysql://' + o['user'] + ':' + o['pass'] + '@localhost/'+o['name'])
 
       if 'deploymentModule' in settings:
         run_drush('en -y '+settings['deploymentModule'])
+
+      reset()
   else:
     print red('Aborting; missing hasDrush, useForDevelopment or supportsInstalls in  '+current_config)
 
