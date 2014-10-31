52a7bfaee5b0b7a48be92584188a91f428c6cf70
muschpusch <volkan@rhizom.nl>
2014-10-31 11:33:41 +0100

    adding stricthostfile option to scp command

--- fabfile.py
+++ fabfile.py
@@ -565,6 +565,10 @@ def _copyDBFrom(config_name = False):
     if source_config['supportsZippedBackups']:
       sql_name_source += '.gz'
 
+    o = source_config['sshTunnel']
+    if 'strictHostKeyChecking' in o:
+      sshArg = ' -o StrictHostKeyChecking=no '
+
     # copy sql to target
     run('scp -P '+str(source_ssh_port)+' '+ssh_args+':'+sql_name_source+' '+sql_name_target+ ' >>/dev/null')
     # cleanup and remove file from source
