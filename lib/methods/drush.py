13e9c6d800e3a7e4a59e97d7f0222aa189ed4e17
Stephan Maximilian Huber <stephan@factorial.io>
2015-12-06 21:25:14 +0100

    Reenable updatedb

--- lib/methods/drush.py
+++ lib/methods/drush.py
@@ -20,7 +20,7 @@ class DrushMethod(BaseMethod):
       with warn_only():
         if configuration.getSettings('deploymentModule'):
           self.run_drush('en -y ' + configuration.getSettings('deploymentModule'))
-      # self.run_drush('updb -y')
+      self.run_drush('updb -y')
       with warn_only():
         if self.methodName == 'drush8':
           self.run_drush('config-import staging -y')
