e7439cbe2b9863e55da8de2ef3c5e5cee7bef902
Stephan Maximilian Huber <stephan@factorial.io>
2016-03-13 19:50:06 +0100

    Force script-output.

--- lib/methods/scripts.py
+++ lib/methods/scripts.py
@@ -43,7 +43,7 @@ class ScriptMethod(BaseMethod):
     env.output_prefix = False
 
     for line in commands:
-      with cd(rootFolder), shell_env(**environment), hide('running'):
+      with cd(rootFolder), shell_env(**environment), hide('running'), show('output'):
         handled = False
         start_p = line.find('(')
         end_p = line.rfind(')')
