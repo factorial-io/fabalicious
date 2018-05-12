64656c5ad7c06f7718ea0e57106495e027793354
D34dMan <shibin@factorial.io>
2018-05-12 05:56:45 +0530

    Provide example Plugin Foo

---
+++ plugins/foo.py
@@ -0,0 +1,13 @@
+import logging
+log = logging.getLogger('fabric.fabalicious.foo')
+from yapsy.IPlugin import IPlugin
+
+
+from fabric.api import *
+from fabric.tasks import Task
+
+class Foo(Task, IPlugin):
+  aliases = ['foo','foobar']
+  def run(self):
+    print "Foo bar"
+    log.debug('?')
